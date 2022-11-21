ce4d920410742d420d63dced39ad61304837c9a6
Glitch (subdued-open-plate) <none>
2022-11-21 01:40:16 +0000

    💰🗾 Checkpoint ./package.json:51299026/6 ./commands/play.js:51299026/457 ./events/ready.js:51299026/43

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
-  await client.user.setActivity("Music", {
-    type: "LISTENING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+  await client.user.setActivity("ba % komak begirid", {
+    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
